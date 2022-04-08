d093757dbdf9912fa82e72cf100045bbdfe10bbf
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 17:03:35 +0200

    1 file modified

--- App.js
+++ App.js
@@ -4,11 +4,6 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
-
 export default function App() {4
     //<Hero />
     return (
