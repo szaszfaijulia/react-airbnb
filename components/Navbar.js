a3546c8254a7c767d2c3f9b4ff5e598762782784
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 16:30:14 +0200

    3 files modified

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react"
 
 export default function Navbar() {
-  return(
-    <nav>
-      <img src="https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/airbnb.png"/>
-    </nav>
-  )
+    return (
+        <nav>
+            <img src="https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/airbnb.png" className="nav--logo"/>
+        </nav>
+    )
 }
