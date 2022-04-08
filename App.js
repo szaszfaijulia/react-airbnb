7dd96268dea864f437bcc8f0b2c047531348241c
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 23:38:57 +0200

    3 files modified, 1 added - props

--- App.js
+++ App.js
@@ -9,7 +9,14 @@ export default function App() {
     return (
         <div>
             <Navbar />
-            <Card />
+            <Card 
+                img="https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/Katie-Zaferes.jpeg"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
--- components/Card.js
+++ components/Card.js
@@ -1,34 +1,31 @@
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
 import React from "react"
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
-
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
-
-export default function Card() {
-    return (
-        <div className="card">
-          <img src="https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/Katie-Zaferes.jpeg" className="card--image"/>
-          <div className="card--stats">
-            <img src="" />
-            <span>5.0</span>
-            <span>(6) • </span>
-            <span>USA</span>
-          </div>
+export default function Card(props) {
+  return (
+      <div className="card">
+        <img src={props.img} className="card--image"/>
+        <div className="card--stats">
+          <img src="https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/PngItem_1524402.png" className="card--star"/>
+          <span>{props.rating}</span>
+          <span className="gray">({props.reviewCount}) • </span>
+          <span className="gray">{props.country}</span>
         </div>
-    )
+        <p>{props.title}</p>
+        <p><span className="bold">From ${props.price}</span> / person</p>
+      </div>
+  )
 }
+
+//src={`../images/${props.img}`}
+//console.log(props)
