682d68336604bd97853b088dc0b0aa9448df68ff
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-08 17:03:43 +0200

    1 file modified

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 
-export default function App() {4
+export default function App() {
     //<Hero />
     return (
         <div>
