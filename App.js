cadf1f02398be8d54b8838340cc034c984f921ef
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-09 12:16:19 +0200

    2 files modified

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import './style.css'
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
--- data.js
+++ data.js
@@ -21,7 +21,7 @@ export default [
       "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
     price: 125,
     coverImg:
-      'https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/Katie-Zaferes.jpeg',
+      'https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/wp9798927.webp',
     stats: {
       rating: 5.0,
       reviewCount: 30,
@@ -36,7 +36,7 @@ export default [
       'Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)',
     price: 50,
     coverImg:
-      'https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/Katie-Zaferes.jpeg',
+      'https://raw.githubusercontent.com/szaszfaijulia/react-airbnb/master/src/bike.jpg',
     stats: {
       rating: 4.8,
       reviewCount: 2,
